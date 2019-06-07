77bc45750ef95411cf4e0dc3cf753b985773086a
Bennett Robinson <bennettrobinson15@gmail.com>
2019-06-07 00:49:17 -0700

    added skeletons for ontology querying module and CLI/driver module

---
+++ driver.py
@@ -0,0 +1,5 @@
+"""
+This module serves as the main entry point for the program. It will house the CLI, asking the 
+   user for a genre, and passing the resultant instrument selections to the machine learning
+   module to generate instrument tracks which will be packaged into a MIDI output file
+"""
---
+++ onto_utils.py
@@ -0,0 +1,4 @@
+"""
+This module will query the OWL Ontology based on a user's inputted genre to 
+   select a set of instruments as midi program integers
+"""
